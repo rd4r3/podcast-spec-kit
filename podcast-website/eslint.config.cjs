1ec045b9412d8f2a55c65f913cb524e016a2f292
engineer83 <r84694415@gmail.com>
2026-04-05 17:22:43 +0200

    Fixed linter config

--- podcast-website/eslint.config.cjs
+++ podcast-website/eslint.config.cjs
@@ -1,11 +1,24 @@
 // eslint.config.cjs
 const next = require('@next/eslint-plugin-next');
+const typescript = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
 
 module.exports = [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
       '@next/next': next,
+      '@typescript-eslint': typescript,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     rules: {
       ...next.configs.recommended.rules,
